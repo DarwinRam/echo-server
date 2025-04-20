fd5cdd1e2a6ddb47f3a4ac8713328e50d7fea578
Darwin Ramos <2014110527@ub.edu.bz>
2025-04-20 14:40:27 -0600

    Added inactivity timeout: Disconnects clients after 30 seconds of no activity.

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
 	clientAddr := conn.RemoteAddr().String()
-	clientIP := strings.Split(clientAddr, ":")[0] // Get IP only (remove port)
+	clientIP := strings.Split(clientAddr, ":")[0]
 
 	fmt.Printf("[+] New connection from %s at %s\n", clientAddr, time.Now().Format(time.RFC3339))
 
@@ -23,7 +23,7 @@ func handleConnection(conn net.Conn) {
 		fmt.Printf("[-] Disconnected: %s at %s\n", clientAddr, time.Now().Format(time.RFC3339))
 	}()
 
-	// Open log file for this client
+	// Open per-client log file
 	logFileName := fmt.Sprintf("%s.log", clientIP)
 	logFile, err := os.OpenFile(logFileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
@@ -35,9 +35,14 @@ func handleConnection(conn net.Conn) {
 	reader := bufio.NewReader(conn)
 
 	for {
+		// 🔥 Set 30-second read timeout
+		conn.SetReadDeadline(time.Now().Add(30 * time.Second))
+
 		message, err := reader.ReadString('\n')
 		if err != nil {
-			if err == io.EOF {
+			if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
+				fmt.Printf("[!] %s disconnected due to inactivity\n", clientAddr)
+			} else if err == io.EOF {
 				fmt.Printf("[!] %s disconnected (EOF)\n", clientAddr)
 			} else {
 				fmt.Printf("[!] Error reading from %s: %v\n", clientAddr, err)
@@ -61,6 +66,7 @@ func handleConnection(conn net.Conn) {
 		}
 	}
 }
+
 
 func main() {
 	// Define a port flag with default value 4000
