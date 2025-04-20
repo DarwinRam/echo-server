7cdd136bb899a6759425c165159e48e35c5a3268
Darwin Ramos <2014110527@ub.edu.bz>
2025-04-20 13:59:32 -0600

    updated the handleConnection to log all messages from each client: Save each client’s messages to a separate file named after their IP address.

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net"
+	"os"
 	"strings"
 	"time"
 )
@@ -13,16 +14,26 @@ func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
 	clientAddr := conn.RemoteAddr().String()
+	clientIP := strings.Split(clientAddr, ":")[0] // Get IP only (remove port)
+
 	fmt.Printf("[+] New connection from %s at %s\n", clientAddr, time.Now().Format(time.RFC3339))
 
 	defer func() {
 		fmt.Printf("[-] Disconnected: %s at %s\n", clientAddr, time.Now().Format(time.RFC3339))
 	}()
 
+	// Open log file for this client
+	logFileName := fmt.Sprintf("%s.log", clientIP)
+	logFile, err := os.OpenFile(logFileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Printf("[!] Failed to open log file for %s: %v\n", clientIP, err)
+		return
+	}
+	defer logFile.Close()
+
 	reader := bufio.NewReader(conn)
 
 	for {
-		// Read until newline
 		message, err := reader.ReadString('\n')
 		if err != nil {
 			if err == io.EOF {
@@ -33,10 +44,15 @@ func handleConnection(conn net.Conn) {
 			return
 		}
 
-		// Trim input
 		trimmed := strings.TrimSpace(message)
 
-		// Echo it back
+		// Log to file
+		logLine := fmt.Sprintf("[%s] %s\n", time.Now().Format(time.RFC3339), trimmed)
+		if _, err := logFile.WriteString(logLine); err != nil {
+			fmt.Printf("[!] Failed to write to log for %s: %v\n", clientIP, err)
+		}
+
+		// Echo back
 		_, err = conn.Write([]byte(trimmed + "\n"))
 		if err != nil {
 			fmt.Printf("[!] Error writing to %s: %v\n", clientAddr, err)
@@ -44,6 +60,7 @@ func handleConnection(conn net.Conn) {
 		}
 	}
 }
+
 
 
 
