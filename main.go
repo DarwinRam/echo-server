5b11272a3aca451b62d11e1ec6f14049a46ac5d0
Darwin Ramos <2014110527@ub.edu.bz>
2025-04-20 14:23:33 -0600

    Added inactivity timeout: Disconnect clients after 30 seconds of no activity.

--- main.go
+++ main.go
@@ -8,13 +8,14 @@ import (
 	"os"
 	"strings"
 	"time"
+	"flag"
 )
 
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
 	clientAddr := conn.RemoteAddr().String()
-	clientIP := strings.Split(clientAddr, ":")[0] // Get IP only (remove port)
+	clientIP := strings.Split(clientAddr, ":")[0]
 
 	fmt.Printf("[+] New connection from %s at %s\n", clientAddr, time.Now().Format(time.RFC3339))
 
@@ -22,7 +23,7 @@ func handleConnection(conn net.Conn) {
 		fmt.Printf("[-] Disconnected: %s at %s\n", clientAddr, time.Now().Format(time.RFC3339))
 	}()
 
-	// Open log file for this client
+	// Open per-client log file
 	logFileName := fmt.Sprintf("%s.log", clientIP)
 	logFile, err := os.OpenFile(logFileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
@@ -34,9 +35,14 @@ func handleConnection(conn net.Conn) {
 	reader := bufio.NewReader(conn)
 
 	for {
+		// 🔥 Set 30-second read timeout
+		conn.SetReadDeadline(time.Now().Add(30 * time.Second))
+
 		message, err := reader.ReadString('\n')
 		if err != nil {
-			if err == io.EOF {
+			if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
+				fmt.Printf("[!] %s disconnected due to inactivity\n", clientAddr)
+			} else if err == io.EOF {
 				fmt.Printf("[!] %s disconnected (EOF)\n", clientAddr)
 			} else {
 				fmt.Printf("[!] Error reading from %s: %v\n", clientAddr, err)
@@ -62,27 +68,29 @@ func handleConnection(conn net.Conn) {
 }
 
 
+func main() {
+	// Define a port flag with default value 4000
+	port := flag.String("port", "4000", "TCP port to listen on")
+	flag.Parse()
 
+	address := fmt.Sprintf(":%s", *port)
 
-func main(){
-
-	listener, err:=net.Listen("tcp", ":4000")
+	listener, err := net.Listen("tcp", address)
 	if err != nil {
-		panic(err)
+		fmt.Printf("[!] Failed to start server on port %s: %v\n", *port, err)
+		os.Exit(1)
 	}
 	defer listener.Close()
 
-	fmt.Println("Server listening on :4000")
+	fmt.Printf("[✓] Server listening on port %s\n", *port)
 
-	for{
+	for {
 		conn, err := listener.Accept()
-		if err != nil{
-			fmt.Println("Error accepting:", err)
+		if err != nil {
+			fmt.Println("[!] Error accepting:", err)
 			continue
 		}
-		go handleConnection(conn)
+		go handleConnection(conn) // concurrency already added!
 	}
-	
-
 }
 
